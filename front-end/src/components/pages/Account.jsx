6c5cfa2de85c16bc3d51e777f1a6aa59649e4357
abreuandre <andreluismachadoabreu@gmail.com>
2026-02-18 17:16:12 -0300

    Atualizando e estilizando arquivo Account.jsx

--- front-end/src/components/pages/Account.jsx
+++ front-end/src/components/pages/Account.jsx
@@ -1,24 +1,37 @@
-import React from 'react'
+import React from "react";
+import { useParams } from "react-router-dom";
 
 const Account = () => {
+  const { subpage } = useParams();
+
+  const buttonClass = (button) => {
+    let finalClass =
+      "hover:bg-primary-400 cursor-pointer rounded-full px-4 py-2 transition hover:text-white";
+
+    if (button === subpage) finalClass += " bg-primary-400 text-white";
+
+    return finalClass;
+  };
+
   return (
     <section className="p-8">
-        <div className="mx-auto flex max-w-7xl flex-col gap-4">
-            <div>
-               <button>Perfil</button>
-               <button>Reservas</button>
-               <button>Lugares</button>
-            </div>
-
-            <div>
-                <p>Locado como Andre (andre@andre.com)</p>
-
-                <button>Logout</button>
-            </div>
-            
+      <div className="mx-auto flex max-w-7xl flex-col items-center gap-4">
+        <div className="flex">
+          <button className={buttonClass("profile")}>Perfil</button>
+          <button className={buttonClass("bookings")}>Reservas</button>
+          <button className={buttonClass("places")}>Lugares</button>
         </div>
-    </section>
-  )
-}
 
-export default Account
+        <div className="flex flex-col items-center gap-2">
+          <p>Locado como Andre (andre@andre.com)</p>
+
+          <button className="bg-primary-400 cursor-pointer rounded-full px-4 py-2 text-white transition">
+            Logout
+          </button>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Account;
