c9551a12da9236c0db4138cd213aff04ec331c3d
abreuandre <andreluismachadoabreu@gmail.com>
2026-02-28 19:23:23 -0300

    Iniciando Aula 7 criando arquivo AccPlaces para criação da tela para adicionar lugares

---
+++ front-end/src/components/AccPlaces.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+const AccPlaces = () => {
+  const addNewPlace = () => {};
+
+  return (
+    <div>
+      <button
+        onClick={addNewPlace}
+        className="bg-primary-400 flex min-w-44 cursor-pointer gap-2 rounded-full px-4 py-2 text-white transition"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="size-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+          />
+        </svg>
+        Adicionar novo lugar
+      </button>
+    </div>
+  );
+};
+
+export default AccPlaces;
--- front-end/src/pages/Account.jsx
+++ front-end/src/pages/Account.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link, useParams } from "react-router-dom";
 import AccProfile from "../components/AccProfile";
+import AccPlaces from "../components/AccPlaces";
 
 const Account = () => {
   const { subpage } = useParams();
@@ -18,12 +19,19 @@ const Account = () => {
     <section className="p-8">
       <div className="mx-auto flex max-w-7xl flex-col items-center gap-8">
         <div className="flex gap-2">
-          <Link to="/account/profile" className={buttonClass("profile")}>Perfil</Link>
-          <Link to="/account/bookings" className={buttonClass("bookings")}>Reservas</Link>
-          <Link to="/account/places" className={buttonClass("places")}>Lugares</Link>
+          <Link to="/account/profile" className={buttonClass("profile")}>
+            Perfil
+          </Link>
+          <Link to="/account/bookings" className={buttonClass("bookings")}>
+            Reservas
+          </Link>
+          <Link to="/account/places" className={buttonClass("places")}>
+            Lugares
+          </Link>
         </div>
 
         {subpage === "profile" && <AccProfile />}
+        {subpage === "places" && <AccPlaces />}
       </div>
     </section>
   );
