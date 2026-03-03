47fd8c585c430ecb37d9523e6742fa26f223199d
abreuandre <andreluismachadoabreu@gmail.com>
2026-03-03 10:52:02 -0300

    Continuação da aula 7 na parte de Comodidades

--- front-end/src/components/NewPlace.jsx
+++ front-end/src/components/NewPlace.jsx
@@ -3,15 +3,23 @@ import { useState } from 'react'
 const NewPlace = () => {
   const [title, setTitle] = useState("");
   const [city, setCity] = useState("");
+  const [photos, setPhotos] = useState("");
+  const [description, setDescription] = useState("");
+  const [perks, setPerks] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
 
   return (
-    <div className="w-full px-8 flex flex-col gap-6">
+    <form onSubmit={handleSubmit} className="w-full px-8 flex flex-col gap-6">
         <div className="flex flex-col gap-1">
-            <h2 className="ml-2 text-2xl font-bold">Título</h2>
+            <label htmlFor="title" className="ml-2 text-2xl font-bold">Título</label>
             <input
                 type="text"
                 placeholder="Digite o título do seu anúncio"
                 className="rounded-full border border-gray-300 px-4 py-2"
+                id="title"
                 value={title}
                 onChange={(e) => setTitle(e.target.value)}
             />
@@ -29,15 +37,16 @@ const NewPlace = () => {
             />
         </div>
         <div className="flex flex-col gap-1">
-            <h2 className="ml-2 text-2xl font-bold">Fotos</h2>
+            <label htmlFor="photos" className="ml-2 text-2xl font-bold">Fotos</label>
 
             <div className="flex gap-2">         
               <input
                 type="text"
                 placeholder="Adicione uma foto pelo link dela"
                 className="rounded-full border border-gray-300 px-4 py-2 grow"
-                value={city}
-                onChange={(e) => setCity(e.target.value)}
+                id="photos"
+                value={photos}
+                onChange={(e) => setPhotos(e.target.value)}
               />
               <button className="hover:bg-gray-200 rounded-full border border-gray-300 px-4 py-2 bg-gray-100 cursor-pointer transition">Enviar foto</button>
             </div>
@@ -53,7 +62,129 @@ const NewPlace = () => {
             </div>
             
         </div>
-    </div>
+
+        <div className="flex flex-col gap-1">
+            <label htmlFor="description" className="ml-2 text-2xl font-bold">Descrição</label>
+            <textarea
+                placeholder="Digite a descrição do seu anúncio"
+                className="rounded-2xl border border-gray-300 px-4 py-2 h-56 resize-none"
+                id="description"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+            />
+        </div>
+
+        <div className="flex flex-col gap-1">
+            <label htmlFor="perks" className="ml-2 text-2xl font-bold">Comodidades</label>
+
+          <div>
+            <label htmlFor="wifi" className="flex gap-2 items-center rounded-xl border border-gray-300 px-4 py-3">
+              <input
+                type="checkbox"
+                id="wifi"
+                value={wifi}
+                onChange={(e) => 
+                  setPerks((prevValue) => [...prevValue, e.target.value])
+                }
+              />
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M8.288 15.038a5.25 5.25 0 0 1 7.424 0M5.106 11.856c3.807-3.808 9.98-3.808 13.788 0M1.924 8.674c5.565-5.565 14.587-5.565 20.152 0M12.53 18.22l-.53.53-.53-.53a.75.75 0 0 1 1.06 0Z" />
+              </svg>
+              Wifi
+            </label>
+
+            <label htmlFor="wifi" className="flex gap-2 items-center rounded-xl border border-gray-300 px-4 py-3">
+              <input
+                type="checkbox"
+                id="wifi"
+                value={wifi}
+                onChange={(e) => 
+                  setPerks((prevValue) => [...prevValue, e.target.value])
+                }
+              />
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M8.288 15.038a5.25 5.25 0 0 1 7.424 0M5.106 11.856c3.807-3.808 9.98-3.808 13.788 0M1.924 8.674c5.565-5.565 14.587-5.565 20.152 0M12.53 18.22l-.53.53-.53-.53a.75.75 0 0 1 1.06 0Z" />
+              </svg>
+              Wifi
+            </label>
+
+            <label htmlFor="wifi" className="flex gap-2 items-center rounded-xl border border-gray-300 px-4 py-3">
+              <input
+                type="checkbox"
+                id="wifi"
+                value={wifi}
+                onChange={(e) => 
+                  setPerks((prevValue) => [...prevValue, e.target.value])
+                }
+              />
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M8.288 15.038a5.25 5.25 0 0 1 7.424 0M5.106 11.856c3.807-3.808 9.98-3.808 13.788 0M1.924 8.674c5.565-5.565 14.587-5.565 20.152 0M12.53 18.22l-.53.53-.53-.53a.75.75 0 0 1 1.06 0Z" />
+              </svg>
+              Wifi
+            </label>
+
+            <label htmlFor="wifi" className="flex gap-2 items-center rounded-xl border border-gray-300 px-4 py-3">
+              <input
+                type="checkbox"
+                id="wifi"
+                value={wifi}
+                onChange={(e) => 
+                  setPerks((prevValue) => [...prevValue, e.target.value])
+                }
+              />
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M8.288 15.038a5.25 5.25 0 0 1 7.424 0M5.106 11.856c3.807-3.808 9.98-3.808 13.788 0M1.924 8.674c5.565-5.565 14.587-5.565 20.152 0M12.53 18.22l-.53.53-.53-.53a.75.75 0 0 1 1.06 0Z" />
+              </svg>
+              Wifi
+            </label>
+
+            <label htmlFor="wifi" className="flex gap-2 items-center rounded-xl border border-gray-300 px-4 py-3">
+              <input
+                type="checkbox"
+                id="wifi"
+                value={wifi}
+                onChange={(e) => 
+                  setPerks((prevValue) => [...prevValue, e.target.value])
+                }
+              />
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M8.288 15.038a5.25 5.25 0 0 1 7.424 0M5.106 11.856c3.807-3.808 9.98-3.808 13.788 0M1.924 8.674c5.565-5.565 14.587-5.565 20.152 0M12.53 18.22l-.53.53-.53-.53a.75.75 0 0 1 1.06 0Z" />
+              </svg>
+              Wifi
+            </label>
+
+            <label htmlFor="wifi" className="flex gap-2 items-center rounded-xl border border-gray-300 px-4 py-3">
+              <input
+                type="checkbox"
+                id="wifi"
+                value={wifi}
+                onChange={(e) => 
+                  setPerks((prevValue) => [...prevValue, e.target.value])
+                }
+              />
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M8.288 15.038a5.25 5.25 0 0 1 7.424 0M5.106 11.856c3.807-3.808 9.98-3.808 13.788 0M1.924 8.674c5.565-5.565 14.587-5.565 20.152 0M12.53 18.22l-.53.53-.53-.53a.75.75 0 0 1 1.06 0Z" />
+              </svg>
+              Wifi
+            </label>
+
+            <label htmlFor="wifi" className="flex gap-2 items-center rounded-xl border border-gray-300 px-4 py-3">
+              <input
+                type="checkbox"
+                id="wifi"
+                value={wifi}
+                onChange={(e) => 
+                  setPerks((prevValue) => [...prevValue, e.target.value])
+                }
+              />
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M8.288 15.038a5.25 5.25 0 0 1 7.424 0M5.106 11.856c3.807-3.808 9.98-3.808 13.788 0M1.924 8.674c5.565-5.565 14.587-5.565 20.152 0M12.53 18.22l-.53.53-.53-.53a.75.75 0 0 1 1.06 0Z" />
+              </svg>
+              Wifi
+            </label>
+          </div>
+        </div>
+    </form>
   )
 }
 
