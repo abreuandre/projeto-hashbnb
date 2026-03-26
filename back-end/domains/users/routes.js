927e70a05cd953d3a76c2b69452c224a919a096c
abreuandre <andreluismachadoabreu@gmail.com>
2026-03-26 00:17:43 -0300

    fix: Consertando os arquivos routes.js de users, jwt.js de utils e Login.jsx

--- back-end/domains/users/routes.js
+++ back-end/domains/users/routes.js
@@ -4,7 +4,7 @@ import { connectDb } from "../../config/db.js";
 import User from "./models.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-import { JWTVerify } from "../../utils/jwtVerify.js";
+import { JWTVerify, JWTSign } from "../../utils/jwt.js";
 
 const router = Router();
 const bcryptSalt = bcrypt.genSaltSync();
@@ -44,11 +44,13 @@ router.post("/", async (req, res) => {
     const { _id } = newUserDoc;
     const newUserObj = { name, email, _id };
 
-    jwt.sign(newUserObj, JWT_SECRET_KEY, {}, (error, token) => {
-      if (error) throw error;
+    try {
+      const token = await JWTSign(newUserObj);
 
       res.cookie("token", token).json(newUserObj);
-    });
+    } catch (error) {
+      res.status(500).json("Erro ao assinar com o JWT", error);
+    }
   } catch (error) {
     res.status(500).json(error);
     throw error;
@@ -69,11 +71,17 @@ router.post("/login", async (req, res) => {
 
       if (passwordCorrect) {
         const newUserObj = { name, email, _id };
-        const token = jwt.sign(newUserObj, JWT_SECRET_KEY);
+        try {
+          const token = await JWTSign(newUserObj);
 
-        console.log({ token, JWT_SECRET_KEY });
+          console.log(token);
+          res.json("Respondido");
 
-        res.cookie("token", token).json(newUserObj);
+          // res.cookie("token", token).json(newUserObj);
+        } catch (error) {
+          console.error(error);
+          res.status(500).json("Erro ao assinar com o JWT");
+        }
       } else {
         res.status(400).json("Senha inválida!");
       }
--- back-end/utils/jwt.js
+++ back-end/utils/jwt.js
@@ -21,3 +21,20 @@ export const JWTVerify = (req) => {
     return null;
   }
 };
+
+export const JWTSign = (newUserObj) => {
+  return new Promise((resolve, reject) => {
+    jwt.sign(
+      newUserObj,
+      JWT_SECRET_KEY,
+      { expiresIn: "1d" },
+      (error, token) => {
+        if (error) {
+          console.error("Deu algum erro ao assinar com o JWT:", error);
+          reject(error);
+        }
+        resolve(token);
+      },
+    );
+  });
+};
