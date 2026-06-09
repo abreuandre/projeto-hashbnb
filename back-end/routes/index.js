b660cd0dee7a55c7cfb0b61a43245f5700e58f6d
abreuandre <andreluismachadoabreu@gmail.com>
2026-06-09 14:58:09 -0300

    Atualização no arquivo index.js em back-end/routes

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
-import UserRoutes from "../domains/users/routes.js";
+import userRoutes from "../domains/users/routes.js";
 import placesRoutes from "../domains/places/routes.js";
-import BookingRoutes from "../domains/bookings/routes.js";
+import bookingRoutes from "../domains/bookings/routes.js";
 
 const router = Router();
 
-router.use("/users", UserRoutes);
+router.use("/users", userRoutes);
 router.use("/places", placesRoutes);
-router.use("/bookings", BookingRoutes);
+router.use("/bookings", bookingRoutes);
 
 export default router;
