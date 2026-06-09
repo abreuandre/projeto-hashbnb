21a68c1ab48adc023f4ec9b0136335ee1d47d067
abreuandre <andreluismachadoabreu@gmail.com>
2026-06-09 14:43:07 -0300

    Atualização de alguns arquivos do back-end sobre Places

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import UserRoutes from "../domains/users/routes.js";
-import PlaceRoutes from "../domains/places/routes.js";
+import placesRoutes from "../domains/places/routes.js";
 import BookingRoutes from "../domains/bookings/routes.js";
 
 const router = Router();
 
 router.use("/users", UserRoutes);
-router.use("/Places", PlaceRoutes);
+router.use("/places", placesRoutes);
 router.use("/bookings", BookingRoutes);
 
 export default router;
